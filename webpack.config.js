956e7d632c2c6827dcec3066418260d3aecd8e2d
Andrey Zatochniy <andreyz11288@gmail.com>
2020-10-19 23:19:22 +0300

    полностью рабочая галерея

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+
+
 module.exports = {
     
     entry: './src/index.js',
@@ -11,10 +13,16 @@ module.exports = {
     },
     module: {
         rules: [{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
-            { test: /\.svg$/, use: 'svg-inline-loader' },
-            { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      }
+            { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },            
+            { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, 'css-loader'] },            
+            {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
         ],
         
 },
