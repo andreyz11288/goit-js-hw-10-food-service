5c891cdb99da494f344234a40fc20c6661e843ab
andreyz11288 <andreyz11288@gmail.com>
2020-10-19 16:01:25 +0300

    5

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         ],
         
 },
-    plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    plugins: [new HtmlWebpackPlugin({ template: 'index.html' }),
    new MiniCssExtractPlugin({
       filename: 'main.css'
     }),],
